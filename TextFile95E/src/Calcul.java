efbb10f14aa71356ec4183468bd32720a69b0026
seb <elusuesebastien@gmail.com>
2018-10-14 23:24:55 +0200

    Calcul added

---
+++ TextFile95E/src/Calcul.java
@@ -0,0 +1,13 @@
+
+public class Calcul {
+
+	public static void main(String[] args) {
+		int Se,Bastien;
+		Se = 100 ;
+	    Bastien = 100 ;
+	    System.out.println("Se+Bastien = "+ (Se+Bastien));
+		// TODO Auto-generated method stub
+
+	}
+
+}
